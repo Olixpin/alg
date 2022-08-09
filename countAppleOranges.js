4e781abcf76644b720ef0655646a1d6675754f61
Olixpin <noibiabdulrasaq@gmail.com>
2022-08-09 11:20:49 +0100

    started kangaroo algorithm challenge

--- countAppleOranges.js
+++ countAppleOranges.js
@@ -2,23 +2,10 @@
 
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
   // Write your code here
-  let appleCount = 0;
-  let orangeCount = 0;
-
-  for (let i = 0; i <= apples.length; i++) {
-    if (a + apples[i] >= s && apples[i] <= t) {
-      appleCount++;
-    }
-  }
-
-  for (let i = 0; i <= oranges.length; i++) {
-    if (b + oranges[i] >= s && b + oranges[i] <= t) {
-      orangeCount++;
-    }
-  }
-
-  console.log(appleCount);
-  console.log(orangeCount);
+  console.log(apples.filter(apple => a + apple >= s && a + apple <= t).length);
+  console.log(
+    oranges.filter(orange => b + orange >= s && b + orange <= t).length
+  );
 }
 
 countApplesAndOranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4]);
