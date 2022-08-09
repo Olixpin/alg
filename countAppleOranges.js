c32fd7e2d4d30d051de3fe4d5d14b1712e62c32c
Olixpin <noibiabdulrasaq@gmail.com>
2022-08-09 01:18:58 +0100

    refined countapple.js file

--- countAppleOranges.js
+++ countAppleOranges.js
@@ -4,17 +4,15 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
   // Write your code here
   let appleCount = 0;
   let orangeCount = 0;
-  let appleFallPosition = apples.map(applePosition => applePosition + a);
-  let orangesFallPosition = oranges.map(orangePosition => orangePosition + b);
 
   for (let i = 0; i <= apples.length; i++) {
-    if (appleFallPosition[i] >= s && appleFallPosition[i] <= t) {
+    if (a + apples[i] >= s && apples[i] <= t) {
       appleCount++;
     }
   }
 
   for (let i = 0; i <= oranges.length; i++) {
-    if (orangesFallPosition[i] >= s && orangesFallPosition[i] <= t) {
+    if (b + oranges[i] >= s && b + oranges[i] <= t) {
       orangeCount++;
     }
   }
