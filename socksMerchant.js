4e3e10de9cd74b887363265c4913252f02aa7c7b
Olixpin <noibiabdulrasaq@gmail.com>
2022-08-16 18:14:07 +0100

    Socks Merchant Challenge Completed

---
+++ socksMerchant.js
@@ -0,0 +1,22 @@
+const socksMerchant = (n, ar) => {
+  if (ar.length !== n) return 0;
+  let myMap = new Map();
+  let socksPairCount = 0;
+
+  for (let i = 0; i < n; i++) {
+    const sock = ar[i];
+    if (!myMap.has(sock)) myMap.set(sock, 1);
+    else {
+      const newCount = myMap.get(sock) + 1;
+      myMap.set(sock, newCount);
+    }
+  }
+
+  for (const [key, value] of myMap) {
+    socksPairCount += Math.floor(value / 2);
+  }
+
+  return socksPairCount;
+};
+
+console.log(socksMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]));
